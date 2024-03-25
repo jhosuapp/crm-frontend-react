9bc4f58b83b6980b0a3ddd9ceb4ce64f39e6763e
jhosuapp <jhosuapvll@gmail.com>
2024-03-25 14:27:04 -0500

    feat: conect to endpoint products

--- src/jsx/components/products/Product.jsx
+++ src/jsx/components/products/Product.jsx
@@ -1,7 +1,37 @@
+//Hooks react
+import { useContext, useEffect, useState } from "react";
+//Global components
 import { Container } from "../global/Container";
 import { Title } from "../global/Title";
+//Context
+import { GlobalContext } from "../../context/GlobalContext";
+//Axios
+import { baseAxios } from "../../config/Axios";
 
 const Product = ()=>{
+    //Global context && states
+    const { globalDelete, setGlobalDelete } = useContext(GlobalContext);
+    const [ stateProducts, setStateProducts, globalError, setGlobalError ] = useState(true);
+    //Conex to endpoint
+    const products = async ()=>{
+        const request = await baseAxios.get('/productos');
+        return request;
+    }
+    //Execute conexion to endpoint
+    useEffect(()=>{
+        products().then(res=>{
+            const { data } = res;
+            if(data){
+                console.log(data);
+                setStateProducts(true);
+            }else{
+                setStateProducts(false);
+            }
+        }).catch(err=>{
+            setStateProducts(false);
+        });
+    }, [ globalDelete ]);
+
     return (
         <Container cls={'container container--bg custom-fonts'}>  
             <Title text_title={'Mis productos'} btn_text="Crear producto" btn_link={'/productos/crear'} />
