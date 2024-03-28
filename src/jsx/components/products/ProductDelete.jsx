82ec90baf09d8815c537254bc3e98c72cfa8379a
jhosuapp <jhosuapvll@gmail.com>
2024-03-28 16:54:01 -0500

    fix: error with modals and global context

--- src/jsx/components/products/ProductDelete.jsx
+++ src/jsx/components/products/ProductDelete.jsx
@@ -1,7 +1,7 @@
 //Axios
 import { baseAxios } from '../../config/Axios';
 //React hooks
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 //Global context
 import { GlobalContext } from '../../context/GlobalContext';
 //Global components
@@ -10,7 +10,8 @@ import { ModalConfirm } from '../global/ModalConfirm';
 
 const ProductDelete = ({ id_product })=>{
     //Global 
-    const { setGlobalDelete, globalModalConfirm, setGlobalModalConfirm  } = useContext(GlobalContext);
+    const { setGlobalDelete, globalModalConfirm, setGlobalModalConfirm, setGlobalModal, globalModal   } = useContext(GlobalContext);
+    const [ error, setError ] = useState(false);
     //Request
     const request = async ()=>{
         const response = await baseAxios.delete(`/productos/${id_product}`);
@@ -20,18 +21,29 @@ const ProductDelete = ({ id_product })=>{
     const deleteProduct = ()=>{
         request().then(res=>{
             const { data } = res;
+            //Validate success register
+            setGlobalModal(true);
             if(data.mensaje == "El Producto se ha eliminado"){
+                setError(false);
                 setGlobalDelete(id_product);
             }else{
-
+                setError(true);
             }
         }).catch(err=>{
-            console.log(err);
+            //Enable modal
+            setGlobalModal(true);
+            setError(true);
         })
     }
 
     return (
         <>
+            <Modal 
+                cls={error ? `${globalModal && 'active'} modal--error` : `${globalModal && 'active'} modal--success`}
+                message={error ? 'No se ha podido crear el producto, intentalo de nuevo más adelante' : 'El producto se ha creado de manera exitosa'}
+                icon={error ? true : false}
+                link={'/productos'}
+            />
             <ModalConfirm 
                 cls={ globalModalConfirm && 'active' }
                 message={'¿Estas seguro de eliminar este producto? esta acción es irreversible'}
