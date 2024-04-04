fe7dcb6a0505c0b6371d90524df32303596bb075
jhosuapp <jhosuapvll@gmail.com>
2024-04-04 15:42:00 -0500

    feat: list client data in a order

---
+++ src/jsx/components/orders/OrderClient.jsx
@@ -0,0 +1,47 @@
+//Hooks react
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+//Axios
+import { baseAxios } from "../../config/Axios";
+
+const OrderClient = ()=>{
+    //Param url
+    const { id } = useParams();
+    //States
+    const [ dataClient, setDataClient ] = useState('');
+    //Request
+    const request = async ()=>{
+        const response = await baseAxios.get(`/clientes/${id}`)
+        return response.data;
+    }
+    useEffect(()=>{
+        request().then((res)=>{
+            setDataClient(res);
+        }).catch(err=>{
+            console.log(err);
+        });
+    },[]);
+
+
+    return (
+        <>
+            <h3>Datos de cliente</h3>
+            <article className="order-client">
+                <div className="item">
+                    <p>Nombre:</p>
+                    <p>{ `${dataClient.nombre} ${dataClient.apellido}` }</p>
+                </div>
+                <div className="item">
+                    <p>Télefono:</p>
+                    <p>{ dataClient.telefono }</p>
+                </div>
+                <div className="item">
+                    <p>Correo:</p>
+                    <p>{ dataClient.email }</p>
+                </div>
+            </article>
+        </>
+    )
+}
+
+export { OrderClient }
--- src/jsx/components/orders/OrderCreate.jsx
+++ src/jsx/components/orders/OrderCreate.jsx
@@ -1,6 +1,19 @@
+//Global components
+import { Container } from "../global/Container";
+import { Title } from "../global/Title";
+//Components order
+import { OrderClient } from './OrderClient';
+
 const OrderCreate = ()=>{
     return (
-        <h2>hola</h2>
+        <>
+            <Container cls={'container container--bg custom-fonts'}>  
+                <Title text_title={'Crear pedido'} cls={'reverse'} btn_text={'volver'} btn_link={'/'} />
+            </Container>
+            <Container cls={'container container--bg custom-fonts custom-input'}>
+                <OrderClient></OrderClient>
+            </Container>
+        </>
     );
 }
 
