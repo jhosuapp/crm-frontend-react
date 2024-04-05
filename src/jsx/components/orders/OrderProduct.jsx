8b8a95eb92100a3fbd8de2dcd1e545f13432bfb0
jhosuapp <jhosuapvll@gmail.com>
2024-04-05 14:56:50 -0500

    feat: add amount for products selecteds

--- src/jsx/components/orders/OrderProduct.jsx
+++ src/jsx/components/orders/OrderProduct.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { baseAxios } from "../../config/Axios";
+import { ErrorMessage } from "../global/ErrorMessage";
 
 const OrderProduct = ()=>{
 
     const [ products, setProducts ] = useState([]);
+    const [ numberProduct, setNumberProduct ] = useState(1);
 
     //Request products
     const request = async ()=>{
@@ -23,7 +25,6 @@ const OrderProduct = ()=>{
             console.log(err)
         });
     }, []);
-
     //Select products
     const handleClickProduct = (e, state)=>{
         const copyProducts = [...products];
@@ -31,11 +32,24 @@ const OrderProduct = ()=>{
         copyProducts[indexProduct].selected = state;
         setProducts(copyProducts);
     }
+    //Sum products
+    const handleClickSum = ()=>{
+        if(numberProduct < 10){
+            setNumberProduct(numberProduct + 1);
+        }
+    }
+    //Rest products
+    const handleClickRest = ()=>{
+        if(numberProduct > 1){
+            setNumberProduct(numberProduct - 1);
+        }
+    }
 
     return (
         <>
             <article>
                 <h3>Selecciona los productos</h3>
+                    {products.length === 0 && <ErrorMessage cls="secondary" text={"No cuentas con productos"} />}                    
                     {
                         products.map((product)=>(
                             <div className={`item ${product.selected && 'inactive'}`} key={product._id}>
@@ -66,6 +80,15 @@ const OrderProduct = ()=>{
                                         <p className="name">{ product.nombre }</p>
                                         <p className="price">${ product.precio }</p>
                                     </div>
+                                </div>
+                                <div className="block-number-product">
+                                    <button onClick={ handleClickRest }>
+                                        -
+                                    </button>
+                                    <p>{ numberProduct }</p>
+                                    <button onClick={ handleClickSum }>
+                                        +
+                                    </button>
                                 </div>
                                 <button className="btn btn--danger" data-product={product._id} onClick={ (e)=>{ handleClickProduct(e, false) } }>Remover producto</button>
                             </div>
