71c658d3e27caac997687080d09b715a577f7100
jhosuapp <jhosuapvll@gmail.com>
2024-04-13 13:57:30 -0500

    feat: deploy config

--- src/jsx/components/orders/Order.jsx
+++ src/jsx/components/orders/Order.jsx
@@ -1,7 +1,27 @@
-import { Container } from "../global/Container"
-import { Title } from "../global/Title"
+//Axios
+import { useEffect } from "react";
+import { baseAxios } from "../../config/Axios";
+//Components
+import { Container } from "../global/Container";
+import { Title } from "../global/Title";
 
 const Order = ()=>{
+
+    const request = async ()=>{
+        const response = await baseAxios.get('/pedidos')
+        return response.data;
+    }
+
+    useEffect(()=>{
+        request()
+            .then(res=>{
+                console.log(res);
+            }).catch(err=>{
+                console.log(err);
+            });
+    }, []);
+
+
     return (
         <>
             <Container cls={'container container--bg custom-fonts'}>  
